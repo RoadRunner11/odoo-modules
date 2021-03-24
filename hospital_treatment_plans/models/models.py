e9f6cdddc7e30c9d732181d5198392f92fbea37b
Akorede Fodilu Olawale <afordeal88@gmail.com>
2021-03-24 15:20:58 +0100

    update treatment plans module

--- hospital_treatment_plans/models/models.py
+++ hospital_treatment_plans/models/models.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class hospital_treatment_plans(models.Model):
-#     _name = 'hospital_treatment_plans.hospital_treatment_plans'
-#     _description = 'hospital_treatment_plans.hospital_treatment_plans'
+class hospital_treatment_plans(models.Model):
+    _name = 'hospital.treatment.plan'
+    _description = 'hospital treatment plans'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
 #     @api.depends('value')
 #     def _value_pc(self):
 #         for record in self:
