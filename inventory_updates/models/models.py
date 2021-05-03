9b4d4bcbd1e62084169b186a188d65f33c804141
Akorede Fodilu Olawale <afordeal88@gmail.com>
2021-05-03 13:43:15 +0100

    add new changes

--- inventory_updates/models/models.py
+++ inventory_updates/models/models.py
@@ -127,8 +127,8 @@ class ProductTemplate(models.Model, CustomImage):
 			count2 = 0
 			count3 = 0
 			for row in varefil_data:
-				if count1 >=10 :
-					break
+				# if count1 >=10 :
+				# 	break
 				data = {}
 				# row = ''.join(row).split(';')
 				data['type'] = 'product'
@@ -155,8 +155,7 @@ class ProductTemplate(models.Model, CustomImage):
 					data['image_1920'] = image
 				if row[18]:
 					group3_id = self.env['product.category'].search([('name', 'like', row[18].strip())])
-
-					_logger.info(row[18].strip())
+					# _logger.info(row[18].strip())
 					if group3_id:
 						data['categ_id'] = group3_id[0]
 					# data['group1'] = group3_id.parent_id.parent_id
@@ -169,23 +168,22 @@ class ProductTemplate(models.Model, CustomImage):
 						product.write(data)
 				else:
 					self.env['product.template'].create(data)
-				_logger.info(data.get('categ_id'))
-				count1 += 1
+				# count1 += 1
 
 			for row in prisfil_data:
-				if count2 >=10 :
-					break
+				# if count2 >=10 :
+				# 	break
 				row = ''.join(row[0]).split(';')
 				default_code = row[1]
 				duplicates = self.search([('default_code', '=', default_code)])
 				if duplicates:
 					for product in duplicates:
 						product.write({'standard_price': float(row[2])})
-				count2 += 1
+				# count2 += 1
 
 			for row in beholdinfg_data:
-				if count3 >=10 :
-					break
+				# if count3 >=10 :
+				# 	break
 				row = ''.join(row).split(';')
 				product_id = 0
 				template_id = 0
@@ -209,7 +207,7 @@ class ProductTemplate(models.Model, CustomImage):
 						new_qty = change_qty.create(data)
 						new_qty.change_product_qty()
 					
-				count3 += 1
+				# count3 += 1
 				
 		_logger.info('Done')
 		return {}
